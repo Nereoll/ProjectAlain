7455e97b4808705259a4c12566d36b9e8a03fbc0
Nereol <pimboliwell.fr@gmail.com>
2025-09-03 20:35:59 +0200

    feat : added all stages + transitions

--- game.py
+++ game.py
@@ -24,6 +24,7 @@ class Game:
 
         # Joueur
         self.player = Player()
+        self.playerSpawn = (WIDTH // 2, HEIGHT // 2)
         self.all_sprites.add(self.player, layer=2)
 
         # Ath
@@ -33,9 +34,9 @@ class Game:
         self.start_time = time.time()
         self.last_spawn = 0
         self.spawn_delay = 3  # premier ennemi toutes les 3 sec
+        self.spawnable = True
         # Score
         self.score = 0
-
 
 
         # Ressources à charger à l'initialisation
@@ -43,31 +44,51 @@ class Game:
         self.shadow1 = pygame.image.load("assets/images/Shadow1.png").convert_alpha()
         self.shadow2 = pygame.image.load("assets/images/Shadow2.png").convert_alpha()
         self.shadow3 = pygame.image.load("assets/images/Shadow3.png").convert_alpha()
-        self.background = pygame.image.load("assets/images/Base_Stage.png").convert()
 
         # On garde un seul sprite shadow et on change son image selon le HP
         self.shadow_sprite = Shadow(self.shadow1, (0, 80))
         self.shadow_sprites = pygame.sprite.LayeredUpdates()
         self.shadow_sprites.add(self.shadow_sprite, layer=4)
 
-
+        self.firstStage = pygame.image.load("assets/images/Base_Stage.png").convert()
         self.secondStage = pygame.image.load("assets/images/Second_Stage.png").convert()
         self.thirdStage = pygame.image.load("assets/images/Third_Stage.png").convert()
-        self.betweenSTage = pygame.image.load("assets/images/Between_Stage.png").convert()
+        self.fourthStage = pygame.image.load("assets/images/Fourth_Stage.png").convert()
+        self.fifthStage = pygame.image.load("assets/images/Fifth_Stage.png").convert()
 
         # Gestion des stages
         self.stage = 1
         self.stage_backgrounds = {
-            1: self.background,
+            1: self.firstStage,
             2: self.secondStage,
             3: self.thirdStage,
+            4: self.fourthStage,
+            5: self.fifthStage
         }
         self.stage_thresholds = {
-            2: 2000,   # score requis pour passer au stage 2
-            3: 4000,   # score requis pour passer au stage 3
+            2: 100,   # score requis pour passer au stage 2
+            3: 400,   # score requis pour passer au stage 3
+            4: 800,   # score requis pour passer au stage 4
+            5: 800,   # score requis pour passer au stage 5
         }
+
+        self.stage_spawns = {
+            1: (WIDTH // 2, HEIGHT // 2),
+            2: (WIDTH // 2, HEIGHT // 2),
+            3: (WIDTH // 2, HEIGHT // 2),
+            4: (100, HEIGHT // 2),
+            5: (100, HEIGHT // 2)
+        }
+
+
         self.stage_changed = False  # évite de répéter clear_stage
 
+        #door
+        self.door_rect = pygame.Rect(WIDTH // 2 - 30, (HEIGHT - 650), 60, 60)
+        self.door_image1 = pygame.image.load("assets/images/Door.png").convert_alpha()
+        self.door_image2 = pygame.image.load("assets/images/Door2.png").convert_alpha()
+        self.door_image3 = pygame.image.load("assets/images/Door3.png").convert_alpha()
+        self.door=False
 
 
     def new(self):
@@ -91,9 +112,8 @@ class Game:
     def update(self):
         """Mise à jour des objets"""
         current_time = time.time() - self.start_time
-
         # Spawner des ennemis au fil du temps
-        if current_time - self.last_spawn >= self.spawn_delay:
+        if current_time - self.last_spawn >= self.spawn_delay and self.spawnable:
             self.spawn_enemy()
             self.last_spawn = current_time
             if self.spawn_delay > 0.8:
@@ -103,12 +123,38 @@ class Game:
         if oldLength > self.enemies.__len__():
             self.score += 100
 
-        # Vérifie si on doit changer de stage
+        #change la porte en fonction du stage
+        if self.stage == 1:
+            self.door_image = self.door_image1
+        elif self.stage == 2:
+            self.door_image = self.door_image2
+        elif self.stage == 3:
+            self.door_image = self.door_image3
+            self.door_rect = pygame.Rect(WIDTH - 80, HEIGHT // 2 , 60, 60)
+        elif self.stage == 4:
+            self.door_rect = pygame.Rect(WIDTH - 80, HEIGHT // 2 , 60, 60)
+
+
+        #Actions spécifiques à la fin du jeu
+        if self.stage > 3:
+            self.spawnable = False #empeche le spawn de nouceaux ennemis
+            self.enemies.empty()
+            self.playerSpawn = (WIDTH // 2, 80) #deplaces le joueur au bon endroit
+
+        # Changes de stage si on touches la porte
         for next_stage, threshold in self.stage_thresholds.items():
             if self.score >= threshold and self.stage < next_stage:
-                self.clear_stage(f"Stage {next_stage}")
-                self.stage = next_stage
+                self.clear_stage()
+                if self.door_rect.colliderect(self.player.rect):
+                    self.stage = next_stage
+                    self.door = False
+                    self.spawnable = True
+
+                    # Repositionner le joueur selon le stage
+                    self.player.rect.center = self.stage_spawns[self.stage]
+                    self.player.mask = pygame.mask.from_surface(self.player.image)  # recalcule la mask collision
                 break
+
 
 
         # Mise à jour explicite de l'ATH
@@ -133,37 +179,20 @@ class Game:
         self.all_sprites.add(enemy, layer=1)
         self.enemies.add(enemy)
 
-    def clear_stage(self, stage_name):
-        """Affiche l'écran de transition entre les stages"""
-        transition_start = time.time()
-        showing = True
-        while showing:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                    return
-
-            self.screen.blit(self.betweenSTage, (0, 0))
-            title_text = self.font_title.render(stage_name, True, WHITE)
-            self.screen.blit(title_text, (WIDTH // 2 - title_text.get_width() // 2,
-                                          HEIGHT // 2 - title_text.get_height() // 2))
-            pygame.display.flip()
-
-            if time.time() - transition_start > 2:  # 2 secondes
-                showing = False
-
-        # Nettoyer les ennemis restants
-        self.spawn_delay=3
-        self.last_spawn=0
+    def clear_stage(self):
+        self.spawnable = False
+        self.spawn_delay = 3
+        self.last_spawn = 0
+        if self.enemies.__len__() == 0:
+            self.door=True
         if self.player.hp <4:
             self.player.hp=4
-        self.enemies.empty()
-
 
     def draw(self):
         """Affichage"""
         # Fond du stage courant
         self.screen.blit(self.stage_backgrounds[self.stage], (0, 0))
+
 
         # Sprites
         if self.player.state == "invisible":
@@ -171,6 +200,9 @@ class Game:
                 self.screen.blit(enemy.question_mark, enemy.question_mark_rect)
 
         self.all_sprites.draw(self.screen)
+
+        if self.door:
+            self.screen.blit(self.door_image, self.door_rect)
 
         # Ombres selon HP
         if self.player.hp == 3:
